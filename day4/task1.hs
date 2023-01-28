3d513d96e9c2928010e43e89e06f5cbebc6bcc23
alordash <alordash@mail.ru>
2023-01-28 23:18:31 +0300

    Part one

---
+++ day4/task1.hs
@@ -0,0 +1,44 @@
+data Range =
+  Range Int Int
+  deriving (Read, Show, Eq)
+
+rangeContainsRange :: Range -> Range -> Bool
+rangeContainsRange (Range beginFirst endFirst) (Range beginSecond endSecond) =
+  beginFirst <= beginSecond && endFirst >= endSecond
+
+rangesOverlap :: Range -> Range -> Bool
+rangesOverlap a b = rangeContainsRange a b || rangeContainsRange b a
+
+inputLoop :: IO [String]
+inputLoop = do
+  line <- getLine
+  if line == "."
+    then return []
+    else do
+      rest <- inputLoop
+      return (line : rest)
+
+splitBy :: (a -> Bool) -> [a] -> [[a]]
+splitBy p l =
+  case dropWhile p l of
+    [] -> []
+    suitable ->
+      case splitBy p rest of
+        []        -> [head]
+        something -> head : something
+      where (head, rest) = break p suitable
+
+stringToRange :: String -> Range
+stringToRange s = toRange $ map read (splitBy (== '-') s)
+  where
+    toRange [x, y] = Range x y
+
+tuplify [a, b] = (a, b)
+
+main = do
+  lines <- inputLoop
+  let rangePairs = map (tuplify . map stringToRange . splitBy (== ',')) lines
+  print rangePairs
+  let overlaps = map (uncurry rangesOverlap) rangePairs
+  let overlapsCount = length $ filter (== True) overlaps
+  print overlapsCount
